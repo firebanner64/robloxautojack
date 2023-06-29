77976118b8f4e687fd750fd5e4d83e12bf85fdc6
painfulpaladins <painfulpaladins@gmail.com>
2023-06-29 19:44:55 +0300

    Upgraded the code and with nice syntax! The cjs need fixing tho

--- generate.py
+++ generate.py
@@ -5,6 +5,19 @@ import time
 import subprocess
 import ctypes
 
+def base(word): #base code for all of the types
+    keyboard.press_and_release("/")
+    time.sleep(0.05)
+    keyboard.write(word,0.03)
+    time.sleep(0.05)
+    keyboard.press_and_release("enter")
+    time.sleep(0.05)
+    keyboard.press("space")
+    time.sleep(0.05)
+    keyboard.release("space")
+    time.sleep(2.3)
+
+
 class GenerateThread(threading.Thread):
     
     def get_id(self):
@@ -25,66 +38,56 @@ class GenerateThread(threading.Thread):
             print('Exception raise failure')
     
 def jj(config_instance):
+    time.sleep(3)
     punctuation = config_instance.punctuation
     dir = config_instance.dir
     for num in range(config_instance.start, config_instance.end + 1):
         output = util.capitalize(config_instance, util.get_number(num)) + punctuation
-        keyboard.wait("space")
-        subprocess.call(["cscript.exe", str(dir) + "\\openchat.vbs", "//NOLOGO"])
-        time.sleep(0.1)
-        keyboard.write(output)
-        keyboard.wait('enter')
+        base(output)
 
 def hj(config_instance):
+    time.sleep(3)
     punctuation = config_instance.punctuation
     dir = config_instance.dir
     for num in range(config_instance.start, config_instance.end + 1):
         output = util.capitalize(config_instance, util.get_number(num)) # no punctuation
         for char in output.replace(' ', ''): # all except last character, no spaces
-            keyboard.wait("space")
-            subprocess.call(["cscript.exe", str(dir) + "\\openchat.vbs", "//NOLOGO"])
-            time.sleep(0.1)
-            keyboard.write(char)
-            keyboard.wait('enter')
-        keyboard.wait("space")
-        subprocess.call(["cscript.exe", str(dir) + "\\openchat.vbs", "//NOLOGO"])
-        time.sleep(0.1)
-        keyboard.write(output + punctuation)
-        keyboard.wait('enter')
+            base(char)
+        base(output+punctuation)
+
+        
 
 def dj(config_instance):
+    time.sleep(3)
     punctuation = config_instance.punctuation
     dir = config_instance.dir
     for num in range(config_instance.start, config_instance.end + 1):
         output = util.capitalize(config_instance, util.get_number(num))[::-1] # no punctuation, reversed
         for char in output.replace(' ', ''): # all except last character, no spaces
-                keyboard.wait("space")
-                subprocess.call(["cscript.exe", str(dir) + "\\openchat.vbs", "//NOLOGO"])
-                time.sleep(0.1)
-                keyboard.write(char)
-                keyboard.wait('enter')
+                base(char)
 
-        keyboard.wait("space")
-        subprocess.call(["cscript.exe", str(dir) + "\\openchat.vbs", "//NOLOGO"])
-        time.sleep(0.1)
-        keyboard.write(output + punctuation)
-        keyboard.wait('enter')
+        base(output + punctuation)
 
 def cj(config_instance):
+    time.sleep(3)
     punctuation = config_instance.punctuation
     dir = config_instance.dir
-    keyboard.wait('enter')
     for num in range(config_instance.start, config_instance.end + 1):
+
+
         output = util.capitalize(config_instance, util.get_number(num)) + punctuation
-        subprocess.call(["cscript.exe", str(dir) + "\\openchat.vbs", "//NOLOGO"])
-        time.sleep(0.1)
-        keyboard.write("/e cheer")
-        keyboard.wait('enter')
-        time.sleep(0.1)
-        subprocess.call(["cscript.exe", str(dir) + "\\openchat.vbs", "//NOLOGO"])
-        time.sleep(0.1)
-        keyboard.write(output)
-        keyboard.wait('enter')
+        keyboard.press_and_release("/")
+        time.sleep(0.05)
+        keyboard.write("/e cheer",0.03)
+        time.sleep(0.05)
+        keyboard.press_and_release('enter')
+        time.sleep(0.05)
+        keyboard.press_and_release("/")
+        time.sleep(0.05)
+        keyboard.write(output,0.03)
+        time.sleep(0.05)
+        keyboard.press_and_release('enter')
+        time.sleep(2.5)
 
 
 
